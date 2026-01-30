3d1d3ace4b218bf2f559200071e6f5112ab845f3
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-01-30 14:10:31 +0530

    Completed patient CRUD functions

--- src/main/java/org/dreamdevzone/hospital/controller/PatientController.java
+++ src/main/java/org/dreamdevzone/hospital/controller/PatientController.java
@@ -1,10 +1,44 @@
 package org.dreamdevzone.hospital.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.dreamdevzone.hospital.model.dto.PatientDto;
+import org.dreamdevzone.hospital.service.PatientService;
+import org.dreamdevzone.hospital.util.ApiResponse;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patient")
+@RequiredArgsConstructor
 public class PatientController {
+    private final PatientService service;
 
+    @PostMapping
+    ResponseEntity<@NotNull ApiResponse<PatientDto>> add(@RequestBody PatientDto patientDto){
+        return ApiResponse.created("Patient created!", service.addPatient(patientDto));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<@NotNull ApiResponse<PatientDto>> get(@PathVariable UUID id){
+        return ApiResponse.success("Patient fetched!", service.getPatient(id));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<@NotNull ApiResponse<List<PatientDto>>> getAll(@PathVariable UUID id){
+        return ApiResponse.success("Fetched patients!", service.getAllPatients());
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<@NotNull ApiResponse<PatientDto>> update(@RequestBody PatientDto patientDto, @PathVariable UUID id){
+        return ApiResponse.success("Patient updated!", service.updatePatient(patientDto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<@NotNull ApiResponse<Void>> remove(@PathVariable UUID id){
+        return ApiResponse.success("Patient removed!");
+    }
 }
