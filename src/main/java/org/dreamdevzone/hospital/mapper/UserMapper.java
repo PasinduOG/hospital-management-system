407b7177ac25e81cacfe9d8a67dbcd7dd8b87229
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-01-30 10:22:16 +0530

    Connect with database using application.yml file

--- src/main/java/org/dreamdevzone/hospital/mapper/UserMapper.java
+++ src/main/java/org/dreamdevzone/hospital/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package org.dreamdevzone.hospital.mapper;
 
 import org.dreamdevzone.hospital.model.dto.UserDto;
 import org.dreamdevzone.hospital.model.entity.User;
+import org.mapstruct.Mapper;
 
 public interface UserMapper {
     UserDto toDto(User user);
