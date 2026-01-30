473a89c80b0e4e97212eeccb63773f214b86b7b8
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-01-30 16:40:20 +0530

    Missing delete method added in DoctorController

--- src/main/java/org/dreamdevzone/hospital/controller/DoctorController.java
+++ src/main/java/org/dreamdevzone/hospital/controller/DoctorController.java
@@ -30,6 +30,7 @@ public class DoctorController {
 
     @DeleteMapping("/{id}")
     ResponseEntity<@NotNull ApiResponse<Void>> deleteDoctor(@PathVariable UUID id){
+        service.deleteDoctor(id);
         return ApiResponse.success("Doctor removed");
     }
 
