efbddb64010626fdb7ce92d3a25a97ad24eb0106
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-01-31 22:34:48 +0530

    Added pagination for LabReportController, removed toDtos from LabReportMapper and added unused suppress warning for LabReportController, and LabReportServiceImpl

--- src/main/java/org/dreamdevzone/hospital/controller/LabReportController.java
+++ src/main/java/org/dreamdevzone/hospital/controller/LabReportController.java
@@ -6,41 +6,44 @@ import org.dreamdevzone.hospital.model.dto.LabReportDto;
 import org.dreamdevzone.hospital.service.LabReportService;
 import org.dreamdevzone.hospital.util.ApiResponse;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/labReport")
+@SuppressWarnings("unused") // For hide unused warnings for endpoint methods @SuppressWarnings("unused")
 public class LabReportController {
     private final LabReportService service;
 
     @PostMapping
-    ResponseEntity<@NotNull ApiResponse<LabReportDto>> addLabReport(@Valid @RequestBody LabReportDto dto){
+    ResponseEntity<@NotNull ApiResponse<LabReportDto>> addLabReport(@Valid @RequestBody LabReportDto dto) {
         return ApiResponse.created("Lab report created!", service.addLabReport(dto));
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<@NotNull ApiResponse<LabReportDto>> searchLabReport(@PathVariable UUID id){
+    ResponseEntity<@NotNull ApiResponse<LabReportDto>> searchLabReport(@PathVariable UUID id) {
         return ApiResponse.success("Lab report fetched!", service.getLabReport(id));
     }
 
     @DeleteMapping("/{id}")
-    ResponseEntity<@NotNull ApiResponse<Void>> deleteLabReport(@PathVariable UUID id){
+    ResponseEntity<@NotNull ApiResponse<Void>> deleteLabReport(@PathVariable UUID id) {
         service.deleteLabReport(id);
         return ApiResponse.success("Lab report removed!");
     }
 
     @GetMapping
-    ResponseEntity<@NotNull ApiResponse<List<LabReportDto>>> getAllLabReports(){
-        return ApiResponse.success("Fetched lab reports!", service.getAllLabReports());
+    ResponseEntity<@NotNull ApiResponse<Page<@NotNull LabReportDto>>> getAllLabReports(Pageable pageable) {
+        String message = String.format("Fetched lab reports! Page number: %d, Page size: %d", pageable.getPageNumber(), pageable.getPageSize());
+        return ApiResponse.success(message, service.getAllLabReports(pageable));
     }
 
     @PutMapping("/{id}")
-    ResponseEntity<@NotNull ApiResponse<LabReportDto>> updateLabReport(@Valid @RequestBody LabReportDto dto, @PathVariable UUID id){
-        return ApiResponse.success("Lab report updated!", service.updateLabReport(dto,id));
+    ResponseEntity<@NotNull ApiResponse<LabReportDto>> updateLabReport(@Valid @RequestBody LabReportDto dto, @PathVariable UUID id) {
+        return ApiResponse.success("Lab report updated!", service.updateLabReport(dto, id));
     }
 }
--- src/main/java/org/dreamdevzone/hospital/mapper/LabReportMapper.java
+++ src/main/java/org/dreamdevzone/hospital/mapper/LabReportMapper.java
@@ -4,11 +4,8 @@ import org.dreamdevzone.hospital.model.dto.LabReportDto;
 import org.dreamdevzone.hospital.model.entity.LabReport;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface LabReportMapper {
     LabReportDto toDto(LabReport dto);
     LabReport toEntity(LabReportDto labReportDto);
-    List<LabReportDto> toEntities(List<LabReport> labReports);
 }
