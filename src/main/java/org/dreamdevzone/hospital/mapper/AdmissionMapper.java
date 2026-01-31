1c4f0919195680c7f48453c655be6a48cba57cfd
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-01-31 20:09:46 +0530

    Added pagination for AdmissionController, removed toDtos from AdmissionMapper and added unused suppress warning for AdmissionController and AdmissionServiceImpl

--- src/main/java/org/dreamdevzone/hospital/mapper/AdmissionMapper.java
+++ src/main/java/org/dreamdevzone/hospital/mapper/AdmissionMapper.java
@@ -4,11 +4,8 @@ import org.dreamdevzone.hospital.model.dto.AdmissionDto;
 import org.dreamdevzone.hospital.model.entity.Admission;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface AdmissionMapper {
     Admission toEntity(AdmissionDto admissionDto);
     AdmissionDto toDto(Admission admission);
-    List<AdmissionDto> toDtos(List<Admission> admissions);
 }
--- src/main/java/org/dreamdevzone/hospital/service/AdmissionService.java
+++ src/main/java/org/dreamdevzone/hospital/service/AdmissionService.java
@@ -1,14 +1,16 @@
 package org.dreamdevzone.hospital.service;
 
 import org.dreamdevzone.hospital.model.dto.AdmissionDto;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface AdmissionService {
     AdmissionDto addAdmission(AdmissionDto dto);
     AdmissionDto getAdmission(UUID uuid);
     void deleteAdmission(UUID uuid);
-    List<AdmissionDto> getAllAdmissions();
+    Page<@NotNull AdmissionDto> getAllAdmissions(Pageable pageable);
     AdmissionDto updateAdmission(AdmissionDto dto,UUID uuid);
 }
