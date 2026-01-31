6ae7d0c53910c6acfa5b1af27fdce0060ea87359
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-01-31 22:03:38 +0530

    Annotated as @NotNull to every entity types and key types in every repository

--- src/main/java/org/dreamdevzone/hospital/repository/DoctorRepository.java
+++ src/main/java/org/dreamdevzone/hospital/repository/DoctorRepository.java
@@ -1,9 +1,10 @@
 package org.dreamdevzone.hospital.repository;
 
 import org.dreamdevzone.hospital.model.entity.Doctor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface DoctorRepository extends JpaRepository<Doctor, UUID> {
+public interface DoctorRepository extends JpaRepository<@NotNull Doctor, @NotNull UUID> {
 }
